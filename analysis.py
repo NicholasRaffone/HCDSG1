d7aa4973be9cfd07117f0d573d6287edb8a0d3c2
nko2005 <nko2005@nyu.edu>
2024-05-03 17:16:11 +0400

    created analysis for comments and titles

--- analysis.py
+++ analysis.py
@@ -22,20 +22,30 @@ class SentimentAnalyzer:
         return df
     #returns the sentiment score of the text
     def get_sentiment(self, text):
+        #checks if the comment body is null or not 
         if pd.isnull(text):
             return np.nan
         else:
             return str(self.analyzer.polarity_scores(text)["compound"])
-    
-    def display_scores(self,df):
+    #creates a histogram of the sentiment scores of comments 
+    def display_scores_comments(self,df):
         scores = df["sentiment"].astype(float)
         plt.hist(scores, bins=20, color='c', edgecolor='black', linewidth=1.2)
         plt.xlabel('Sentiment Score')
         plt.ylabel('Frequency')
         plt.title('Sentiment Analysis of True Crime Comments')
         plt.show()
+    def display_scores_titles(self,df):
+        scores = df["sentiment"].astype(float)
+        plt.hist(scores, bins=20, color='c', edgecolor='black', linewidth=1.2)
+        plt.xlabel('Sentiment Score')
+        plt.ylabel('Frequency')
+        plt.title('Sentiment Analysis of True Crime Titles')
+        plt.show()
+    
 if __name__ == "__main__":
     sa = SentimentAnalyzer()
-    df = sa.analyze_sentiment_comments("truecrime_comments.csv")
-    print(df)
-    sa.display_scores(df)
+    df_comments = sa.analyze_sentiment_comments("truecrime_comments.csv")
+    df_titles = sa.analyze_sentiment_title("truecrime_submissions_with_fields.csv")
+    sa.display_scores_comments(df_comments)
+    sa.display_scores_titles(df_titles)
